2cd71836a8273776f2100b444806580dd9abb108
GitShark <53007941+Nikolai-Aleksandrovich@users.noreply.github.com>
2021-03-20 23:11:16 +0800

    receive information from view an trans into Driver.class

--- src/main/java/com/compass/security/DriverRegistrationForm.java
+++ src/main/java/com/compass/security/DriverRegistrationForm.java
@@ -1,6 +1,8 @@
 package com.compass.security;
 
+import com.compass.domain.Driver;
 import lombok.Data;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author Yuyuan Huang
@@ -8,5 +10,15 @@ import lombok.Data;
  */
 @Data
 public class DriverRegistrationForm {
+
+    private String driverName;
+    private String passWord;
+    private String driverLicense;
+    private String carPlate;
+
+    public Driver toDriver(PasswordEncoder passwordEncoder){
+        return new Driver(driverName,passwordEncoder.encode(passWord),driverLicense,carPlate);
+    }
+
 
 }
