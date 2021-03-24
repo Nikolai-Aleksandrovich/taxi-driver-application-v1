f3ba65b9618950e2edf1ac7acab134d7ec121b5b
GitShark <53007941+Nikolai-Aleksandrovich@users.noreply.github.com>
2021-03-24 23:44:05 +0800

    replace simple registerPathController with addviewcontroller

---
+++ src/main/java/com/compass/controller/RegisterPathController.java
@@ -0,0 +1,16 @@
+package com.compass.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author Yuyuan Huang
+ * @create 2021-03-20 15:12
+ */
+//@Controller
+//public class RegisterPathController {
+//    @GetMapping("/registerPath")
+//    public String registerSource(){
+//        return "registerPath";
+//    }
+//}
