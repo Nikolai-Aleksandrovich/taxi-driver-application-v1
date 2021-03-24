f056ba0feb5bf31cb047a09647e2f018e85de5cf
GitShark <53007941+Nikolai-Aleksandrovich@users.noreply.github.com>
2021-03-24 23:41:44 +0800

    replace simple homeController with addviewController in webconfig

--- src/main/java/com/compass/controller/HomeController.java
+++ src/main/java/com/compass/controller/HomeController.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author Yuyuan Huang
  * @create 2021-03-17 20:53
  */
-@Controller
-@RequestMapping
-public class HomeController {
-    @GetMapping("/")
-    public String home(){
-        return "home";
-    }
-}
+//@Controller
+//@RequestMapping
+//public class HomeController {
+//    @GetMapping("/")
+//    public String home(){
+//        return "home";
+//    }
+//}
