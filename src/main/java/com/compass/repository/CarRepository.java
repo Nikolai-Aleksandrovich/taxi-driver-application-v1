01e337aaf4a8eeb5468e3940107e94f737899007
GitShark <53007941+Nikolai-Aleksandrovich@users.noreply.github.com>
2021-03-29 23:06:02 +0800

    change class from CD to C

--- src/main/java/com/compass/repository/CarRepository.java
+++ src/main/java/com/compass/repository/CarRepository.java
@@ -1,13 +1,12 @@
 package com.compass.repository;
 
-import com.compass.domain.CarDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.compass.domain.Car;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * @author Yuyuan Huang
  * @create 2021-03-18 10:58
  */
-public interface CarRepository extends CrudRepository<CarDetails,Long> {
+public interface CarRepository extends CrudRepository<Car,Long> {
 
 }
