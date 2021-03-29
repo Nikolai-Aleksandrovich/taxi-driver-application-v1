3e08783199616b2cfb95bb548455d50cd460771e
GitShark <53007941+Nikolai-Aleksandrovich@users.noreply.github.com>
2021-03-29 23:06:22 +0800

    Repository for Destination

---
+++ src/main/java/com/compass/repository/DestinationRepository.java
@@ -0,0 +1,13 @@
+package com.compass.repository;
+
+import com.compass.domain.Destination;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author Yuyuan Huang
+ * @create 2021-03-24 18:15
+ */
+public interface DestinationRepository extends CrudRepository<Destination,Long> {
+//    Destination save(Destination destination);
+
+}
