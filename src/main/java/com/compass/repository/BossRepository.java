db8990d7c29baec4115307cb0d94320032035884
GitShark <53007941+Nikolai-Aleksandrovich@users.noreply.github.com>
2021-04-04 22:29:22 +0800

    add repository for boss

--- src/main/java/com/compass/repository/BossRepository.java
+++ src/main/java/com/compass/repository/BossRepository.java
@@ -2,11 +2,13 @@ package com.compass.repository;
 
 import com.compass.domain.Boss;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author Yuyuan Huang
  * @create 2021-03-19 18:00
  */
+@Repository
 public interface BossRepository extends CrudRepository<Boss,Long> {
     Boss findBossByBossName(String name);
 }
