29975b7387142144b8606ccf8884604f225020e2
GitShark <53007941+Nikolai-Aleksandrovich@users.noreply.github.com>
2021-04-04 22:28:49 +0800

    mistake by requiredconstruction

--- src/main/java/com/compass/domain/User.java
+++ src/main/java/com/compass/domain/User.java
@@ -15,31 +15,47 @@ import javax.persistence.Id;
 import javax.swing.*;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * @author Yuyuan Huang
  * @create 2021-03-18 11:15
  */
 @Data
+@NoArgsConstructor(access = AccessLevel.PUBLIC,force = true)
 @RequiredArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PRIVATE,force = true)
 @Entity
 public class User implements UserDetails {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     //依赖数据库自动生成Id值
-    private long id;
+    private Long id;
     private final String userName;
     private final String passWord;
     private final String fullName;
+//    private HashMap<String,String> home = new HashMap<>();
     private final String street;
+    private final String district;
     private final String city;
     private final String province;
     private final String phoneNumber;
     private final String latitude;
     private final String longitude;
-
+//
+//    public User(String userName, String encode, String fullName, String street,String district, String city, String province, String phoneNumber, String latitude, String longitude) {
+//        this.userName = userName;
+//        this.passWord = encode;
+//        this.fullName = fullName;
+//        this.street = street;
+//        this.district = district;
+//        this.city = city;
+//        this.province = province;
+//        this.phoneNumber = phoneNumber;
+//        this.latitude = latitude;
+//        this.longitude = longitude;
+//    }
 
 
     @Override
